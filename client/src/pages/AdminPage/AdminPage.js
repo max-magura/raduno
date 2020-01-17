34ce9ac7753584595de71b4d661efd8aabc99494
Austin Graves <austingraves106@aol.com>
2020-01-16 20:55:23 -0600

    Added in the re-route functionality to prevent a user that isn't logged in from using the app

--- client/src/pages/AdminPage/AdminPage.js
+++ client/src/pages/AdminPage/AdminPage.js
@@ -3,6 +3,11 @@ import AdminNav from '../../components/AdminNav/AdminNav';
 import EventCards from '../../components/EventCards/EventCard';
 
 class AdminPage extends Component {
+  componentDidMount() {
+    if (sessionStorage.getItem('userInfo') === null) {
+      window.location.href = '/';
+    }
+  }
   render() {
     return (
       <div>

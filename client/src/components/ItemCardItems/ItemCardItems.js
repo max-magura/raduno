b585e3547c02248574b374a6b94d7ed3dba23fdd
Max-M <maxnmagura@gmail.com>
2020-01-17 21:43:27 -0600

    fixed rsvp form

--- client/src/components/ItemCardItems/ItemCardItems.js
+++ client/src/components/ItemCardItems/ItemCardItems.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Card, Row, Col, Container} from 'react-bootstrap';
+import {Card, Row, Col} from 'react-bootstrap';
 import './ItemCardItems.css'
 
 class ItemCardItems extends Component {
   render() {
     return (
-      <Card.Body className="item-card-item cardBody text-center">
+      <Card.Body className="item-card-item text-center">
         
         <Row>
         <Col className="food-name ">
